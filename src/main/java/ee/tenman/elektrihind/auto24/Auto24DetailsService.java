8a1d4ab568cebcb87f1c322641da91543c10bf57
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-12-08 07:57:34 +0200

    Refactor

--- src/main/java/ee/tenman/elektrihind/auto24/Auto24DetailsService.java
+++ src/main/java/ee/tenman/elektrihind/auto24/Auto24DetailsService.java
@@ -42,15 +42,7 @@ public class Auto24DetailsService {
         String captchaToken = recaptchaSolverService.solveCaptcha();
         log.info("Car captcha solved for regNr: {}", regNr);
         executeJavaScript("document.getElementById('g-recaptcha-response').innerHTML = arguments[0];", captchaToken);
-        acceptCookies = $(By.id("onetrust-accept-btn-handler"));
-        if (acceptCookies.exists()) {
-            acceptCookies.click();
-        }
         $("button[type='submit']").click();
-        acceptCookies = $(By.id("onetrust-accept-btn-handler"));
-        if (acceptCookies.exists()) {
-            acceptCookies.click();
-        }
         ElementsCollection rows = $$("table.result tr");
         Map<String, String> carDetails = new HashMap<>();
         for (int i = 0; i < rows.size(); i++) {

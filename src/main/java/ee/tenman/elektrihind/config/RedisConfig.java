747fff6b102a97c4364d5922cb819ec94b8133e0
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-12-10 00:13:49 +0200

    Reset cache

--- src/main/java/ee/tenman/elektrihind/config/RedisConfig.java
+++ src/main/java/ee/tenman/elektrihind/config/RedisConfig.java
@@ -13,29 +13,21 @@ import java.util.Map;
 @Configuration
 public class RedisConfig {
 
-    public static final String ONE_HOUR_CACHE = "one-hour-cache-2";
-    public static final String THIRTY_MINUTES_CACHE = "thirty-minutes-cache-2";
-    public static final String ONE_DAY_CACHE_1 = "one-day-cache-1";
-    public static final String ONE_DAY_CACHE_2 = "one-day-cache-2";
-    public static final String ONE_DAY_CACHE_3 = "one-day-cache-3";
-    public static final String ONE_YEAR_CACHE_1 = "one-year-cache-1";
-    public static final String ONE_YEAR_CACHE_2 = "one-year-cache-2";
-    public static final String ONE_YEAR_CACHE_3 = "one-year-cache-3";
-    public static final String MESSAGE_COUNTS_CACHE = "messageCounts-2";
+    public static final String ONE_DAY_CACHE_1 = "one-day-cache-11";
+    public static final String ONE_DAY_CACHE_2 = "one-day-cache-22";
+    public static final String ONE_DAY_CACHE_3 = "one-day-cache-33";
+    public static final String ONE_YEAR_CACHE_1 = "one-year-cache-11";
+    public static final String MESSAGE_COUNTS_CACHE = "messageCounts-22";
 
     private static final Duration DEFAULT_TTL = Duration.ofMinutes(10);
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-        cacheConfigurations.put(ONE_HOUR_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1)));
-        cacheConfigurations.put(THIRTY_MINUTES_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(30)));
         cacheConfigurations.put(ONE_DAY_CACHE_1, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1)));
         cacheConfigurations.put(ONE_DAY_CACHE_2, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1)));
         cacheConfigurations.put(ONE_DAY_CACHE_3, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1)));
         cacheConfigurations.put(ONE_YEAR_CACHE_1, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(365)));
-        cacheConfigurations.put(ONE_YEAR_CACHE_2, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(365)));
-        cacheConfigurations.put(ONE_YEAR_CACHE_3, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(365)));
         cacheConfigurations.put(MESSAGE_COUNTS_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1)));
         RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig().entryTtl(DEFAULT_TTL);
         return RedisCacheManager.builder(connectionFactory)

3df1a36c0d3dab4e4012d635592d5b81b0216f44
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-12-12 20:52:11 +0200

    Add more logging

--- src/main/java/ee/tenman/elektrihind/car/PlateDetectionService.java
+++ src/main/java/ee/tenman/elektrihind/car/PlateDetectionService.java
@@ -4,11 +4,13 @@ import ee.tenman.elektrihind.car.easyocr.EasyOcrService;
 import ee.tenman.elektrihind.car.openai.OpenAiVisionService;
 import ee.tenman.elektrihind.car.vision.GoogleVisionService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class PlateDetectionService {
 
     @Resource
@@ -21,9 +23,35 @@ public class PlateDetectionService {
     private OpenAiVisionService openAiVisionService;
 
     public Optional<String> detectPlate(byte[] image) {
-        return easyOcrService.getPlateNumber(image)
-                .or(() -> googleVisionService.getPlateNumber(image))
-                .or(() -> openAiVisionService.getPlateNumber(image));
-    }
+        log.debug("Starting plate detection. Image size: {} bytes", image.length);
+        try {
+            Optional<String> plateNumber = easyOcrService.getPlateNumber(image);
+            if (plateNumber.isPresent()) {
+                log.info("Plate detected by EasyOcrService: {}", plateNumber.get());
+                return plateNumber;
+            }
+            log.debug("EasyOcrService did not detect the plate, trying GoogleVisionService");
 
+            plateNumber = googleVisionService.getPlateNumber(image);
+            if (plateNumber.isPresent()) {
+                log.info("Plate detected by GoogleVisionService: {}", plateNumber.get());
+                return plateNumber;
+            }
+            log.debug("GoogleVisionService did not detect the plate, trying OpenAiVisionService");
+
+            plateNumber = openAiVisionService.getPlateNumber(image);
+            if (plateNumber.isPresent()) {
+                log.info("Plate detected by OpenAiVisionService: {}", plateNumber.get());
+                return plateNumber;
+            }
+            log.debug("OpenAiVisionService did not detect the plate");
+
+            return Optional.empty();
+        } catch (Exception e) {
+            log.error("Error during plate detection", e);
+            return Optional.empty();
+        } finally {
+            log.debug("Plate detection process completed");
+        }
+    }
 }

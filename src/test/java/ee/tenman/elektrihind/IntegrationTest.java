64a21659ea61d8093c03dee1c5d4ff8716336bfd
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-12-17 18:08:38 +0200

    Refactor IntegrationTest to use clearer variable names and fix formatting.

--- src/test/java/ee/tenman/elektrihind/IntegrationTest.java
+++ src/test/java/ee/tenman/elektrihind/IntegrationTest.java
@@ -33,30 +33,30 @@ class RedisInitializer implements ApplicationContextInitializer<ConfigurableAppl
     private static final DockerImageName REDIS_IMAGE = DockerImageName.parse("redis:7.2.3-alpine");
     private static final DockerImageName RABBITMQ_IMAGE = DockerImageName.parse("rabbitmq:3.9-management-alpine");
 
-    private static final GenericContainer<?> rabbitMQContainer = new GenericContainer<>(RABBITMQ_IMAGE)
+    private static final GenericContainer<?> RABBIT_MQ_CONTAINER = new GenericContainer<>(RABBITMQ_IMAGE)
             .withExposedPorts(5672, 15672);
 
-    private static final GenericContainer<?> redisContainer = new GenericContainer<>(REDIS_IMAGE)
+    private static final GenericContainer<?> REDIS_CONTAINER = new GenericContainer<>(REDIS_IMAGE)
             .withExposedPorts(6379)
             .withCommand("redis-server", "--requirepass", CUSTOM_PASSWORD)
             .withEnv("RABBITMQ_DEFAULT_USER", CUSTOM_USERNAME)
             .withEnv("RABBITMQ_DEFAULT_PASS", CUSTOM_PASSWORD);
 
     static {
-        redisContainer.start();
-        rabbitMQContainer.start();
+        REDIS_CONTAINER.start();
+        RABBIT_MQ_CONTAINER.start();
     }
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
                 applicationContext,
-                "spring.data.redis.host=" + redisContainer.getHost(),
-                "spring.data.redis.port=" + redisContainer.getFirstMappedPort(),
+                "spring.data.redis.host=" + REDIS_CONTAINER.getHost(),
+                "spring.data.redis.port=" + REDIS_CONTAINER.getFirstMappedPort(),
                 "spring.data.redis.password=" + CUSTOM_PASSWORD,
 
-                "spring.rabbitmq.host=" + rabbitMQContainer.getHost(),
-                "spring.rabbitmq.port=" + rabbitMQContainer.getMappedPort(5672),
+                "spring.rabbitmq.host=" + RABBIT_MQ_CONTAINER.getHost(),
+                "spring.rabbitmq.port=" + RABBIT_MQ_CONTAINER.getMappedPort(5672),
                 "spring.rabbitmq.username=" + CUSTOM_USERNAME,
                 "spring.rabbitmq.password=" + CUSTOM_PASSWORD
         );

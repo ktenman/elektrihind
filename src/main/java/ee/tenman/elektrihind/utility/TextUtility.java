709cccc5e5fb26690f1423d0f8e28b1b5a1bda9c
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-12-20 17:45:08 +0200

    Improve markdown escaping in TextUtility
    The TextUtility class has been updated to include additional functionality to handle markdown escaping. Special characters have been added to the REPLACEMENTS map, and the escapeMarkdown method now checks for null or empty inputs. An iterative approach has been replaced with a forEach lambda for better readability and efficiency.

--- src/main/java/ee/tenman/elektrihind/utility/TextUtility.java
+++ src/main/java/ee/tenman/elektrihind/utility/TextUtility.java
@@ -22,26 +22,27 @@ public class TextUtility {
             Map.entry("{", "\\{"),
             Map.entry("}", "\\}"),
             Map.entry(".", "\\."),
-            Map.entry("!", "\\!")
+            Map.entry("!", "\\!"),
+            Map.entry("<", "\\<")
     );
 
     public static String escapeMarkdown(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+
         StringBuilder escapedText = new StringBuilder(text);
 
-        for (Map.Entry<String, String> entry : REPLACEMENTS.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-
+        REPLACEMENTS.forEach((key, value) -> {
             int index = escapedText.indexOf(key);
             while (index != -1) {
                 escapedText.replace(index, index + key.length(), value);
                 index += value.length(); // Move to the end of the replacement
                 index = escapedText.indexOf(key, index);
             }
-        }
+        });
 
         return escapedText.toString();
     }
-
 
 }

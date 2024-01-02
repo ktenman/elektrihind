c6922b5d34788033333f8e79b4c82f0f5133df2c
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-01-02 21:31:03 +0200

    Improve PredictService with duration logging
    An enhancement has been made to the PredictService by introducing logging that keeps track of the duration of each prediction. This comes as a useful tool for tracking performance and tracing issues in case the prediction process takes unusually long. TimeUtility class is also imported to aid in the computation of prediction duration.

--- src/main/java/ee/tenman/elektrihind/car/predict/PredictService.java
+++ src/main/java/ee/tenman/elektrihind/car/predict/PredictService.java
@@ -1,5 +1,6 @@
 package ee.tenman.elektrihind.car.predict;
 
+import ee.tenman.elektrihind.utility.TimeUtility;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Backoff;
@@ -17,8 +18,12 @@ public class PredictService {
 
     @Retryable(maxAttempts = 2, backoff = @Backoff(delay = 1500))
     public Optional<String> predict(PredictRequest request) {
+        long predictionStartTime = System.nanoTime();
+        log.info("Predicting {}", request);
         try {
             PredictResponse predictResponse = captchaSolverClient.predict(request);
+            String predictionDuration = TimeUtility.durationInSeconds(predictionStartTime).asString();
+            log.info("Predicted {} in {} seconds", predictResponse, predictionDuration);
             return Optional.ofNullable(predictResponse).map(PredictResponse::getPredictedText);
         } catch (Exception e) {
             log.error("Failed to predict", e);

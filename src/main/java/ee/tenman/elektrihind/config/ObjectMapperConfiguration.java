eda610f29e587a62f7aaf37b8bea196d958c2ba9
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-01-08 11:05:09 +0200

    Updated application config and object mapping
    The application configuration has been updated to allow the captcha-solver URL to be dynamic. Furthermore, Jackson object mapper configuration has been updated in ObjectMapperConfiguration.java by adding a deserialization feature to ignore unknown properties during the deserialization process.

--- src/main/java/ee/tenman/elektrihind/config/ObjectMapperConfiguration.java
+++ src/main/java/ee/tenman/elektrihind/config/ObjectMapperConfiguration.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
 @Configuration
 public class ObjectMapperConfiguration {
 
     public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
-            .registerModule(new JavaTimeModule());
+            .registerModule(new JavaTimeModule())
+            .configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     @Bean
     @Primary

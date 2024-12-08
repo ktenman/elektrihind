89fd04da60a169c89860c50f361eacd31dbe80a5
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-12-08 16:40:37 +0200

    Fix pipeline

---
+++ src/test/java/ee/tenman/elektrihind/car/CarSearchServiceTest.java
@@ -0,0 +1,12 @@
+package ee.tenman.elektrihind.car;
+
+import ee.tenman.elektrihind.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class CarSearchServiceTest {
+
+    @Test
+    void searchV2() {
+    }
+}
